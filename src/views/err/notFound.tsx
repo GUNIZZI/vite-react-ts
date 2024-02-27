d8e256d943bdd946647011a1679752bba0e918de
h2dlhs <h2dlhs@realsn.com>
2024-02-27 23:32:48 +0900

    Error 라우터 설정

---
+++ src/views/err/notFound.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
+import Style from './notFound.module.scss';
+
+interface OwnProps {
+    status: string;
+}
+
+const NotFound = ({ status = '' }) => {
+    const nav = useNavigate();
+
+    return (
+        <div className={Style.wrapper}>
+            <h1>
+                Error <strong>{status}</strong>
+            </h1>
+
+            <div className={Style.btns}>
+                <button onClick={() => nav('/')}>홈</button>
+                <button>돌아가기</button>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
