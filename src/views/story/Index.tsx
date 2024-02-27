e720f4d97999610c3135b283d03ad279acb59936
h2dlhs <h2dlhs@realsn.com>
2024-02-28 00:05:22 +0900

    Firebase 테스트

--- src/views/story/Index.tsx
+++ src/views/story/Index.tsx
@@ -1,20 +1,26 @@
 import { db } from '@/firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, Timestamp } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 
+interface I_Story {
+    user: string;
+    date: Timestamp;
+    title: string;
+    content: string;
+}
+
 const Index = () => {
-    const [test, setTest] = useState();
+    const [test, setTest] = useState<I_Story>({ user: '', date: Timestamp.now(), title: '', content: '' });
 
     // async - await로 데이터 fetch 대기
     async function getTest() {
         //     // document에 대한 참조 생성
-        const docRef = doc(db, 'story', '1');
+        const docRef = doc(db, 'story', '2');
         //     // 참조에 대한 Snapshot 쿼리
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-            console.log(docSnap.data());
-            // setTest(docSnap.data());
+            setTest(docSnap.data() as I_Story);
         }
     }
 
@@ -24,7 +30,10 @@ const Index = () => {
 
     return (
         <>
-            <div style={{ height: '5000px' }}>asdasd</div>
+            <div>FB user: {test?.user}</div>
+            <div>FB date: {test.date.toDate().toString()}</div>
+            <div>FB title: {test?.title}</div>
+            <div>FB content: {test?.title}</div>
         </>
     );
 };
