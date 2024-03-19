e8bfd7f7b00c8b71a18e392d5a1566f3ed5791d9
h2dlhs <h2dlhs@realsn.com>
2024-03-20 01:11:45 +0900

    PrivateGuard 기능 추가

--- src/app/router/PrivateGuard.tsx
+++ src/app/router/PrivateGuard.tsx
@@ -1,49 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 import { userStore } from '../store/user';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 const PrivateGuard = () => {
-    // const { name } = userStore();
-
-    // useEffect(() => {
-    //     const checkAuthentication = async () => {
-    //         while (!!name === false) {
-    //             await new Promise((resolve) => setTimeout(resolve, 100));
-    //         }
-    //         return <Outlet />;
-    //     };
-    //     checkAuthentication();
-    // }, [name]);
-    // useEffect(() => {
-    //     if (!userStore.getState().getUser()) alert('관리자만 접근 가능한 페이지 입니다.');
-    // }, []);
-
-    // return userStore.getState().getUser() ? <Outlet /> : <Navigate to="/login" replace />;
-
     const { name } = userStore();
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     useEffect(() => {
-        const checkAuthentication = async () => {
-            while (!name) {
-                await new Promise((resolve) => setTimeout(resolve, 100));
-            }
-            setIsAuthenticated(true);
-        };
-        checkAuthentication();
-    }, [name]);
+        if (!!name === false) {
+            alert('관리자만 접근 가능한 페이지 입니다.');
+        }
+    }, []);
 
-    console.log('guard');
-
-    if (!isAuthenticated) {
-        console.log('go null');
-        // Wait until authentication is checked
-        return null; // or a loading indicator
-    }
-
-    console.log('go outlet');
-    // If authenticated, render the Outlet
-    return <Outlet />;
+    return !!name == true ? <Outlet /> : <Navigate to="../" replace />;
 };
 
 export { PrivateGuard };
